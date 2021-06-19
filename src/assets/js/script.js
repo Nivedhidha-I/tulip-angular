3daadef4c8fabd967d1f8660d7957e3da5529b73
Nivedhidha-I <sinivi177@gmail.com>
2021-06-19 19:41:51 +0530

    product page design completed changes to be made for each product

---
+++ src/assets/js/script.js
@@ -0,0 +1,11 @@
+$(function()
+    {
+        $(".xzoom,.xzoom-gallery") .xzoom({
+            zoomWidth:450,
+            zoomHeight:450,
+            tint: "#333",
+            Xoffset : 5,
+        
+            
+        });
+    });
